6e7d531e73eb7e6539dba9f2708f82586d65fadc
Beining Zhou <cathyzhou0317@gmail.com>
2019-11-08 09:28:53 -0500

    new

--- Amicabilibus+RemoveDuplicates/src/Amicabilibus.java
+++ Amicabilibus+RemoveDuplicates/src/Amicabilibus.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class Amicabilibus {
@@ -7,7 +8,6 @@ public class Amicabilibus {
 	public static boolean[] checked = new boolean[upperbound+1];
 	
 	public static int sumFactors(int n) {
-	    
 		int sum = 1;
 		for(int i=2;i <= (int)(Math.sqrt(n)); i++){
 	    	if(n%i == 0){
@@ -18,11 +18,28 @@ public class Amicabilibus {
 	    return sum;
 	}
 	
+//	public static int sumFactors(int n) {
+//		int sum = 1;
+//		int original = n;
+//		for(int i=2; i<=n; i++) {
+//			int product = 1;
+//			while(n%i == 0) {
+//				product=product*i+1;
+//				n/=i;
+//			}
+//			sum*=product;
+//		}
+//		if(n>1) {sum*=1+n;}
+//		sum -= original;
+//		return sum;
+//	}
+	
 	public static void main(String[] args) {
 		
 		for(int n=0; n<=upperbound; n++) {
 			factorSums.add(sumFactors(n));
 		}
+		System.out.println("done");
 		
 		for(int i=0; i<=upperbound; i++) {
 			int j = factorSums.get(i);
@@ -34,6 +51,7 @@ public class Amicabilibus {
 				}
 			}
 		}
+		System.out.println(factorSums.get(879712));
 	}
 }
 
